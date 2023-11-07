1c7fc15b9f7e26803fa533a01011c5cedb40fc4c
rekurrenzk <Fatihburak@protonmail.com>
2023-11-07 19:57:50 +0300

    small project

---
+++ FIFO/main.py
@@ -0,0 +1,54 @@
+from collections import deque
+
+class PrintQueue:
+    def __init__(self):
+        self.queue = deque()
+        self.priority_queue = deque()
+
+    def submit_print_job(self, document):
+        self.queue.append(document)
+        print(f"Submitted print job: {document}")
+
+    def submit_priority_job(self, document):
+        self.priority_queue.append(document)
+        print(f"Submitted priority print job: {document}")
+
+    def run_print_job(self):
+
+        if self.priority_queue:
+            current_job = self.priority_queue.popleft()
+            print(f"printing priority job: {current_job}")
+
+        elif self.queue:
+            current_job = self.queue.popleft()
+            print(f"printing: {current_job}")
+
+        else:
+            print("No print jobs in the queue.")
+
+
+    def show_queue(self):
+        print("\nRegular Print Queue:\n")
+        for count, job in enumerate(self.queue, start=1):
+            print(f"{count}. {job}")
+
+    def show_priority_queue(self):
+
+        print("\nPriority Print Queue:")
+        for count, job in enumerate(self.priority_queue, start=1):
+            print(f"{count}. {job}")
+
+
+
+print_queue = PrintQueue()
+print_queue.submit_print_job("Document1.pdf")
+print_queue.submit_print_job("Document2.pdf")
+
+print_queue.show_queue()
+
+print_queue.submit_print_job("alsonot.pdf")
+print_queue.submit_priority_job("this.pdf")
+print_queue.submit_print_job("not.pdf")
+print_queue.show_queue()
+
+print_queue.show_priority_queue()
---
+++ FIFO/readme.txt
@@ -0,0 +1,13 @@
+What I have tried in LIFO, we will not be doing it here:
+
+
+    A queue is a collection where the first element added is the first one to be removed
+
+    FIFO stands for "first-in, first-out"
+
+    For large datasets, using list to implement a queue is not efficient.
+
+    Instead, we should use 'collections' module for 'deque' to pull and push from both ends
+
+
+
